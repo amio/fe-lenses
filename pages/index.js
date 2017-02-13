aefc0e25a761d58d5c7073966c8500c4ea1df252
Amio <amio.cn@gmail.com>
2017-02-13 14:42:53 +0800

    Add GA config.

--- pages/index.js
+++ pages/index.js
@@ -11,6 +11,13 @@ export default () => {
     <Head>
       <title>Full-frame E-mount lenses catalog</title>
       <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+      <script dangerouslySetInnerHTML={{__html: `
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+        ga('create', 'UA-4646421-12', 'auto');
+        ga('send', 'pageview');`}} />
     </Head>
     <header>
       <h1>Full-frame E-mount lenses catalog</h1>
